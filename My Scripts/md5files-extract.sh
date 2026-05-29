36009bbb452c3266dd500ddeaad2f6e09a62b87a
Paul McQuade <paulmcquad@gmail.com>
2026-05-29 17:08:08 +0100

    My Scripts - Extract MD5
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ My Scripts/md5files-extract.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+## Extract just the names
+
+#ls -al ~/Books/Computer/Admin > extract.txt
+#cut -b 50- extract.txt > im.txt
+#rm extract.txt
+#rm im.txt
+
+## Get names and md5sum
+
+md5sum ~/Books/Computer/Admin/* > md5sum.txt
